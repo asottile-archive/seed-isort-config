2fb88dd031f4aba0ff850b2cf007b9c3e28126b7
Anthony Sottile <asottile@umich.edu>
2018-05-14 14:15:07 -0700

    v0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='seed_isort_config',
     description='Statically populate the `known_third_party` `isort` setting.',
     url='https://github.com/asottile/seed-isort-config',
-    version='0.2.0',
+    version='0.3.0',
     author='Anthony Sottile',
     author_email='asottile@umich.edu',
     classifiers=[
